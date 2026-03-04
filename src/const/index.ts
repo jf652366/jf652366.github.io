415f228650087e64071525972fed69abaeb2624d
jf652366 <73322420+jf652366@users.noreply.github.com>
2026-03-04 08:02:15 +0000

    目录优化

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,21 +1,15 @@
 export const nav =[
 	{
-		text: '开始',
+		text: '首页',
 		link: '/'
 	},
-	{
-		text: '笔记',
-		link: '/docs/notes/01-常用网站'
-	}
 ]
 //
 export const sidebar = {
 	'/': [
 		{
-			text: '开始',
-			items: [
-				{ text: '首页', link: '/' }
-			]
+			text: '常用网站',
+			link: '/docs/notes/01-常用网站',
 		}
 	]
 }
